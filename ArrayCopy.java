82a08fe07d98f7b54abaed58200ffb0372e7633f
hiroshi <kohama1619@yahoo.co.jp>
2021-09-12 00:32:51 +0900

    array-2

--- ArrayCopy.java
+++ ArrayCopy.java
@@ -1,5 +1,38 @@
 public class ArrayCopy {
     public static void main(String[] args) {
+        // arr1 = arr2 操作时 2者参照的地址值相同，所以修改arr2值时也会修改arr1，
+        // 所谓的 浅拷贝
+        int[] arr1 = new int[]{1,3,5,7,9};
+        int[] arr2;
+        arr2 = arr1;
+
+        // 深拷贝
+        int[] arr3 = new int[arr1.length];
+        for(int i=0; i<arr1.length; i++){
+            arr3[i] = arr1[i];
+        }
+
+        String[] a = new String[]{"AA", "BB", "CC", "DD", "EE"};
+        // 数组的复制
+        String[] a1 = new String[a.length];
+        for(int i=0; i<a.length; i++){
+            a1[i] = a[i];
+        }
+
+        // 数组的反转
+        String[] a2 = new String[a.length];
+        for(int i=0; i<a.length; i++){
+            a2[i] = a[a.length-i-1];
+        }
+        System.out.println("Original: ");
+        for(int i=0; i<a.length; i++){
+            System.out.print(a[i]+" ");
+        }
+        System.out.println("\n"+"Reversing: ");
+        for(int i=0; i<a2.length; i++){
+            System.out.print(a2[i]+" ");
+        }
+
         
     }
 }
---
+++ DichotomySearch.java
@@ -0,0 +1,25 @@
+public class DichotomySearch {
+    public static void main(String[] args) {
+        int[] arr = new int[]{-98,-34,2,34,54,66,79,105,210,222};
+        int dest = 210;
+        int head = 0; // 初始的首索引
+        int end = arr.length - 1; // 初始的末索引
+        
+        boolean isFlag = true;
+        while(head<end){
+            int middle = (head+end)/2;
+            if(dest == arr[middle]){
+                System.out.println("找到了指定的元素，位置为："+middle);
+                isFlag = false;
+                break;
+            }else if(arr[middle] > dest){
+                end = middle - 1;
+            }else{
+                head = middle + 1;
+            }
+        }
+        if(isFlag){
+            System.out.println("没找到指定的元素");
+        }
+    }
+}
